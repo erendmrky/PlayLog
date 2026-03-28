05eddd52bc0f3f89c32dc1da78305332f8d01718
Burak Demirkaya <buraqdmrky@gmail.com>
2026-03-28 21:20:24 +0300

    resolved review changes

--- src/main/java/com/buren/playlog/service/AbstractService.java
+++ src/main/java/com/buren/playlog/service/AbstractService.java
@@ -1,14 +1,15 @@
 package com.buren.playlog.service;
 
+import com.buren.playlog.model.BaseEntity;
 import com.buren.playlog.repository.AbstractRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public abstract class AbstractService<T, ID> {
+public abstract class AbstractService<T extends BaseEntity, ID> {
 
-    private final AbstractRepository<T, ID> abstractRepository;
+    protected final AbstractRepository<T, ID> abstractRepository;
 
-    public AbstractService(AbstractRepository<T, ID> abstractRepository) {
+    protected AbstractService(AbstractRepository<T, ID> abstractRepository) {
         this.abstractRepository = abstractRepository;
     }
 }
