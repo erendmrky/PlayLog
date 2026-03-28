92e47f698b1892b6b5217a92cac1958c33a466b2
erendmrky <eren.demirkaya01@gmail.com>
2026-03-28 19:59:45 +0300

    initial commit

---
+++ src/main/java/com/buren/playlog/PlaylogApplication.java
@@ -0,0 +1,13 @@
+package com.buren.playlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buren/playlog/PlaylogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buren.playlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
